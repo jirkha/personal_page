1351cafae58f746df483fd116049a4657f8a3a75
jirkha <jirkha13@gmail.com>
2025-04-02 13:55:18 +0300

    fix bug in Navbar component to link in smaller devices

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -33,65 +33,61 @@ function Navbar() {
   const [navbar, setNavbar] = useState<boolean>(false);
 
   return (
-    <div>
-      <header
-        className="w-full bg-black
+    <header
+      className="w-full bg-black
         md:bg-gradient-to-r from-black to-neutral-800"
-      >
-        <nav className="flex items-center justify-between shadow max-w-[1220px] mx-auto">
-          <Image
-            className="ml-8 w-28"
-            src={logo}
-            alt="Logo of the author"
-          ></Image>
-          <ul className="hidden text-xl lg:flex space-x-8 mr-10 text-slate-100">
-            {items.map(({ label, page }) => (
-              <li
+    >
+      <nav className="flex items-center justify-between shadow max-w-[1220px] mx-auto">
+        <Image
+          className="ml-8 w-28"
+          src={logo}
+          alt="Logo of the author"
+        ></Image>
+        <ul className="hidden text-xl lg:flex space-x-8 mr-10 text-slate-100">
+          {items.map(({ label, page }) => (
+            <li
+              className="p-3 hover:text-amber-100 hover:underline decoration-1 cursor-pointer"
+              key={page}
+            >
+              <a
+                href={`https://www.jirivecko.cz/#${page}`}
                 className="p-3 hover:text-amber-100 hover:underline decoration-1 cursor-pointer"
-                key={page}
               >
-                <a
-                  href={`https://www.jirivecko.cz/#${page}`}
-                  className="p-3 hover:text-amber-100 hover:underline decoration-1 cursor-pointer"
-                >
-                  {label}
-                </a>
-              </li>
-            ))}
-          </ul>
-          <div
-            onClick={() => setNavbar(!navbar)}
-            className="flex lg:hidden mr-10 my-8 z-10 cursor-pointer"
-          >
-            {navbar ? (
-              <AiOutlineClose color="white" size={30} />
-            ) : (
-              <AiOutlineMenu color="white" size={30} />
-            )}
-          </div>
-        </nav>
-        {navbar && (
-          <ul className="flex flex-col justify-center items-center bg-slate-800">
-            {items.map(({ label, page }) => (
-              <li
-                className="text-slate-100 mt-4 mb-4 p-3 cursor-pointer"
-                key={page}
+                {label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div
+          onClick={() => setNavbar(!navbar)}
+          className="flex lg:hidden mr-10 my-8 z-10 cursor-pointer"
+        >
+          {navbar ? (
+            <AiOutlineClose color="white" size={30} />
+          ) : (
+            <AiOutlineMenu color="white" size={30} />
+          )}
+        </div>
+      </nav>
+      {navbar && (
+        <ul className="flex flex-col justify-center items-center bg-slate-800">
+          {items.map(({ label, page }) => (
+            <li
+              className="text-slate-100 mt-4 mb-4 p-3 cursor-pointer"
+              key={page}
+            >
+              <a
+                href={`https://www.jirivecko.cz/#${page}`}
+                className="p-3 hover:text-amber-100 hover:underline decoration-1 cursor-pointer"
               >
-                <Link
-                  //onClick={() => setNavbar(!navbar)}
-                  to={page}
-                  smooth
-                  duration={500}
-                >
-                  {label}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        )}
-        <hr className="border-black md:border-neutral-800"></hr>
-      </header>
-    </div>
+                {label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+      <hr className="border-black md:border-neutral-800"></hr>
+    </header>
   );
 }
 
