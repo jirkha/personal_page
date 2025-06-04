13b760777021a44db6d78ff01024cd0d77310d2b
jirkha <jirkha13@gmail.com>
2025-06-04 14:19:52 +0100

    add appio cv and delete others

--- app/data/companyData.ts
+++ app/data/companyData.ts
@@ -125,79 +125,13 @@ export const companyData: { [key: string]: CompanyData } = {
       div: ["Python, Django, PostgreSQL"],
     },
   },
-  mediaboard: {
-    name: "Mediaboard",
+  appio: {
+    name: "Appio",
     profession: "Junior Full-Stack Developer",
-    logo: "mediaboard.svg",
-    companyLink:
-      "https://mediaboard.com/cs/volne-pozice/junior-python-developer/?_gl=1*1jbbjt3*_up*MQ..*_ga*MzcxOTM1ODg1LjE3NDYzNzkyMzk.*_ga_F1KVHNZSSD*czE3NDYzNzkyMzkkbzEkZzEkdDE3NDYzNzkyNTIkajAkbDAkaDE0OTE2NTM4ODE.",
-    bgColor:
-      "radial-gradient(circle, rgb(13, 97, 255, 0.1), rgb(13, 97, 255, 0.8)",
-    textColor: "rgb(13, 97, 255)",
-    imgClass: "mx-auto h-full sm:mt-3 sm:w-3/5 lg:w-2/5",
-    about:
-      "I have been a transport specialist. Since 2021, I have been learning programming. I create my own projects (Python, React) and further develop my skills in the field. Now, I am looking for a job opportunity in ",
-    techStack: {
-      backend: ["Python", "Django", "REST API", "PostgreSQL"],
-      frontend: [
-        "Next.js",
-        "TypeScript",
-        "JavaScript",
-        "React",
-        "Redux",
-        "HTML",
-        "Tailwind CSS",
-        "Material UI",
-        "Axios",
-      ],
-
-      tools: ["Git & GitHub"],
-    },
-    projectTech: {
-      jp: [
-        "Python, Django, PostgreSQL, REST API, JavaScript, React, Redux, HTML, Material UI, Axios",
-      ],
-      suma: [
-        "Next.js, JavaScript, TypeScript, React, HTML, Google API, HTML, Tailwind CSS",
-      ],
-      div: ["Python, Django, PostgreSQL"],
-    },
-  },
-  oksystem: {
-    name: "OKsystem",
-    profession: "Junior Full-Stack Developer",
-    logo: "ok.svg",
-    companyLink: "https://www.oksystem.com/cs/kariera",
-    bgColor:
-      "radial-gradient(circle, rgb(1, 150, 69, 0.1), rgb(1, 150, 69, 0.5)",
-    textColor: "rgb(1, 150, 69)",
-    imgClass: "mx-auto lg:ml-auto h-24",
-    about:
-      "I have been a transport specialist. Since 2021, I have been learning programming. I create my own projects (JavaScript, Python) and further develop my skills in the field. Now, I am looking for a job opportunity in ",
-    techStack: {
-      frontend: [
-        "JavaScript, TypeScript, React, Redux, HTML, Tailwind CSS, Next.js, Material UI, Axios",
-      ],
-      backend: ["Python", "REST API", "Django", "PostgreSQL"],
-      tools: ["Git & GitHub"],
-    },
-    projectTech: {
-      jp: [
-        "JavaScript, React, Python, Django, REST API, PostgreSQL, HTML, Redux, Material UI, Axios",
-      ],
-      suma: [
-        "JavaScript, TypeScript, React, HTML, Next.js, Google API, HTML, Tailwind CSS",
-      ],
-      div: ["Python, Django, PostgreSQL"],
-    },
-  },
-  contember: {
-    name: "Contember",
-    profession: "Junior Full-Stack Developer",
-    logo: "contember.svg",
-    companyLink: "https://www.contember.com/",
-    bgColor: "radial-gradient(circle, rgb(0, 24, 47, 0.5), rgb(0, 24, 47, 0.8)",
-    textColor: "rgb(0, 24, 47)",
+    logo: "appio.svg",
+    companyLink: "https://www.appio.dev/",
+    bgColor: "radial-gradient(circle, rgb(10, 85, 215, 0.4), rgb(10, 85, 215, 0.7)",
+    textColor: "rgb(10, 85, 215)",
     imgClass: "mx-auto lg:mx-0 -mt-4 lg:ml-auto h-24 lg:w-2/5",
     about:
       "I have been a transport specialist. Since 2021, I have been learning programming. I create my own projects (JavaScript, Python) and further develop my skills in the field. Now, I am looking for a job opportunity in ",
@@ -215,32 +149,6 @@ export const companyData: { [key: string]: CompanyData } = {
       suma: [
         "React, JavaScript, TypeScript, HTML, Next.js, Google API, HTML, Tailwind CSS",
       ],
-      div: ["Python, Django, PostgreSQL"],
-    },
-  },
-  aim: {
-    name: "Aim",
-    profession: "Junior Full-Stack Developer",
-    logo: "aim.svg",
-    companyLink: "https://www.startupjobs.cz/nabidka/89491/fullstack-engineer",
-    bgColor:
-      "radial-gradient(circle, rgb(68, 0, 204, 0.3), rgb(68, 0, 204, 0.5)",
-    textColor: "rgb(68, 0, 204)",
-    imgClass: "mx-auto lg:mx-0 lg:-mt-10 lg:ml-auto h-40 lg:w-2/5",
-    about:
-      "I have been a transport specialist. Since 2021, I have been learning programming. I create my own projects (React, Next.js, Python) and further develop my skills in the field. Now, I am looking for a job opportunity in ",
-    techStack: {
-      frontend: [
-        "React, JavaScript, TypeScript, Next.js, Tailwind CSS, Material UI, HTML, Redux, Axios",
-      ],
-      backend: ["Python", "REST API", "Django", "PostgreSQL"],
-      tools: ["Git & GitHub"],
-    },
-    projectTech: {
-      jp: [
-        "React, JavaScript, Python, Django, REST API, PostgreSQL, Material UI, HTML, Redux, Axios",
-      ],
-      suma: ["React, TypeScript, Next.js, Tailwind CSS, HTML, Google API"],
       div: ["Python, Django, PostgreSQL"],
     },
   },
